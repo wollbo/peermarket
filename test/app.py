fd890d631704217cbde08c756c115052d119757c
Pontus <36114799+prokie@users.noreply.github.com>
2022-07-29 09:49:37 +0200

    Fixed pylint issues in app.py

--- test/app.py
+++ test/app.py
@@ -1,29 +1,44 @@
-from flask import Flask, request, jsonify
+"""
+Test
+"""
 import dotenv
+from flask import Flask, jsonify, request
+from flask.logging import create_logger
 
-app = Flask(__name__)
+APP = Flask(__name__)
+LOG = create_logger(APP)
 
 
-@app.before_request
+@APP.before_request
 def log_request_info():
-    app.logger.debug('Headers: %s', request.headers)
-    app.logger.debug('Body: %s', request.get_data())
+    """
+    Test
+    """
+    LOG.debug("Headers: %s", request.headers)
+    LOG.debug("Body: %s", request.get_data())
 
 
-@app.route('/callback', methods=['GET']) # expand to general callback with any arg
+@APP.route("/callback", methods=["GET"])  # expand to general callback with any arg
 def callback():
+    """
+    Test
+    """
     args = request.args
     env = dotenv.find_dotenv()
     arg = args["account_verification_report_id"]
-    dotenv.set_key(env, 'ACCOUNT_VERIFICATION_REPORT_ID', arg)
+    dotenv.set_key(env, "ACCOUNT_VERIFICATION_REPORT_ID", arg)
 
     return jsonify(dict(data=arg))
 
-@app.route('/webhook', methods=['POST']) # expand to general callback with any arg
-def callback():
+
+@APP.route("/webhook", methods=["POST"])  # expand to general callback with any arg
+def post_callback():
+    """
+    Test
+    """
     args = request.args
     return jsonify(dict(args))
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='localhost', port='3000', threaded=True)
+if __name__ == "__main__":
+    APP.run(debug=True, host="localhost", port="3000", threaded=True)
